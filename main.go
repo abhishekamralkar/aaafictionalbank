32bf7425915bace785a480d78d469526fc7dbd45
Abhishek Amralkar <abhishekamralkar@gmail.com>
2024-01-15 21:48:22 -0800

    [aaafb] fix bug and add .gitignore

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	fmt.Printf("The current balance is %.02f\n", app.Balance())
 	app.Deposit(1000)
-	fmt.Printf("Balance after deposit of % .2f%.02f\n", app.Balance())
+	fmt.Printf("Balance after deposit of %.02f\n", app.Balance())
 	app.Withdraw(200)
 	fmt.Printf("Balance after the withdrawal %.02f\n", app.Balance())
 }
