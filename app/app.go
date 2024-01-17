a467a84f374a66315b5e1ab6e59bec0519f98190
Abhishek Amralkar <abhishekamralkar@gmail.com>
2024-01-16 20:20:08 -0800

    [aaafb] add if condition in withdraw

--- app/app.go
+++ app/app.go
@@ -1,15 +1,22 @@
 package app
 
+import "fmt"
+
 var balance float64 = 0
 
 func Balance() float64 {
 	return balance
 }
 
-func Deposit(m float64) {
+func Deposit(m float64) float64 {
 	balance += m
+	return balance
 }
 
-func Withdraw(m float64) {
+func Withdraw(m float64) float64 {
+	if balance < m {
+		fmt.Println("Insufficient Funds")
+	}
 	balance -= m
+	return balance
 }
